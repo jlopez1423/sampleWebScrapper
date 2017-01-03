363557c8de183af45973f9cc7c1e278a905830a3
jlopez1423 <jlopez46@ucsc.edu>
2017-01-02 21:27:08 -0800

    Refactoring code

--- scrapper2/spiders/quotes.py
+++ scrapper2/spiders/quotes.py
@@ -4,6 +4,8 @@ import scrapy
 class QuotesSpider(scrapy.Spider):
 	name = "quotes"
 
+
+	''' This can be written in short hand as
 	def start_requests(self):
 		urls = [
 			'http://quotes.toscrape.com/page/1/',
@@ -13,6 +15,14 @@ class QuotesSpider(scrapy.Spider):
 		for url in urls:
 			yield scrapy.Request(url=url, callback=self.parse)
 
+	This:'''
+
+	start_urls = [
+		'http://quotes.toscrape.com/page/1/',
+		'http://quotes.toscrape.com/page/2/',
+	]
+
+	#parse is scrapy's default callback
 	def parse(self, response):
 		page = response.url.split("/")[-2]
 		filename = 'quotes-%s.html' % page
